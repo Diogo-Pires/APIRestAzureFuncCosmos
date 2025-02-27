ecfb234a65a9b3018c90c0b0fb1385a26748716c
Diogo <diogowebmaster@hotmail.com.com>
2025-02-27 19:07:29 +0000

    Adding presentation tests

--- APIRestAzureFuncCosmos/Application/Interfaces/ITaskService.cs
+++ APIRestAzureFuncCosmos/Application/Interfaces/ITaskService.cs
@@ -6,8 +6,8 @@ public interface ITaskService
 {
     Task<List<TaskDTO>> GetAllAsync(CancellationToken cancellationToken);
     Task<TaskDTO?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
-    Task<Result<TaskDTO>> CreateAsync(TaskDTO createTaskDto, CancellationToken cancellationToken);
-    Task<Result<TaskDTO?>> UpdateAsync(TaskDTO createTaskDto, CancellationToken cancellationToken);
+    Task<Result<TaskDTO>> CreateAsync(TaskDTO taskDto, CancellationToken cancellationToken);
+    Task<Result<TaskDTO?>> UpdateAsync(TaskDTO taskDto, CancellationToken cancellationToken);
     Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken);
     Task<Result> AssignTaskToUserAsync(Guid taskId, string email, CancellationToken cancellationToken);
 }
